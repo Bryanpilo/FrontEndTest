7303f5243a29ca09dc9bcf73977915d3794c012f
Bryan Espinoza <Bryan.Espinoza@cr.ey.com>
2020-02-07 08:38:58 -0600

    Adding isPalindromeMethod

--- src/javascript-test/palindrome.js
+++ src/javascript-test/palindrome.js
@@ -1,1 +1,31 @@
 /* Export your NodeJS module from this file */
+
+const isPalindrome = (palindrome) => {
+
+    if(typeof palindrome === 'string'){
+        var obj = {
+            date:  new Date(),
+            message: "Type of says that it's a diffent kind of type",
+          };
+        
+        return obj;
+    }
+
+    const array = palindrome.split("");
+
+    if(array.length===0)
+        return false;
+
+    let flag = true;
+    
+    for (let i = 1; i < array.length; i++) {
+        if (array[i - 1] !== array[array.length - i]) {
+            flag = false;
+            break;
+        }
+    }
+
+    return flag;
+}
+
+module.exports.isPalindrome= isPalindrome;
